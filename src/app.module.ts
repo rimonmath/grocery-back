2122792c9ea5e8393dcaed90689f922ad3fe6b9b
Mamunur Rashid <rimonmath@gmail.com>
2026-04-10 06:29:53 +0600

    Initial setup with db config

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    // Load .env file
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // Async configuration for TypeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true, // Automatically find our @Entity files
+        synchronize: true, // Set to false in production!
+      }),
+    }),
+  ],
+})
+export class AppModule {}
